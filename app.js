4dbb48d1d2141b726613ede7b5b368ad0a9d8d58
Timur Kalandarov <tkalandarov@usf.edu>
2021-02-07 23:11:34 -0500

    Home update 1
    Added Appearance section and Footer

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/indexController.js
+++ routes/indexController.js
@@ -6,7 +6,7 @@ router.get("/", function (req, res, next) {
   res.locals.css = ["index.css"];
   res.locals.js = ["index.js"];
   res.locals.preloadContent = "<div><h1>Welcome!</h1></div>"
-  res.render("index.hbs", { title: "Express"});
+  res.render("index.hbs", { title: "Home - Timur Kalandarov"});
 });
 
 module.exports = router;
