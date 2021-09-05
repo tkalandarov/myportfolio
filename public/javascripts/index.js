ff96102859aa89fb73760d180b4895361f2b1b78
Timur Kalandarov <tkalandarov@usf.edu>
2021-09-04 21:17:32 -0400

    2.0 Update! Full redesign

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,20 @@
-window.addEventListener("load", () => {
-  document.querySelector("body").classList.add("loaded");
+$(document).ready(function () {
+  $("body").addClass("loaded");
+
+  var parallaxItem = $('.parallax')[0];
+
+  $('.section__inner').mousemove(function (e) {
+    parallax(e, parallaxItem);
+  });
+
+  function parallax(e, target) {
+
+    var x = ($(".section__inner").width() - target.offsetWidth) / 2 - (e.pageX - ($(".section__inner").width() / 2)) / 20;
+    var y = ($(".section__inner").height() - target.offsetHeight) / 2 - (e.pageY - ($(".section__inner").height() / 2)) / 20;
+  
+    $(target).offset({
+      top: y,
+      left: x+50,
+    });
+  };
 });
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,14 +5,7 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.locals.css = ["index.css"];
   res.locals.js = ["index.js"];
-  res.locals.preloadContent = "<div><h1>Welcome!</h1></div>"
-  res.render("index.hbs", { title: "Home - Timur Kalandarov"});
-});
-
-/* GET bio page. */
-router.get("/biography", function (req, res, next) {
-  res.locals.css = ["biography.css"];
-  res.render("biography.hbs", { title: "Biography - Timur Kalandarov"});
+  res.render("index.hbs", { title: "Timur Kalandarov - Software Developer"});
 });
 
 module.exports = router;
