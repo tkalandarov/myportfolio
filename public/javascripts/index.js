726d3ec3a352800e9cf03a1f88d706f0804e44a3
Timur Kalandarov <tkalandarov@usf.edu>
2021-12-01 19:14:58 -0500

    Filled "Works" section and updated resume

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,18 @@
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
 $(document).ready(function () {
   $("body").addClass("loaded");
 
